4949176d738566d3fb09880db5e8691b2d95bc31
pallavijawanjal <pallavij@gmail.com>
2022-08-17 22:23:32 +0530

    Add project files.

---
+++ StockManagement/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    class Node
+    {
+        public string symbol;
+        public double units;
+        public double price;
+        public string type;
+        public string date;
+        public Node next;
+
+        public Node(string symbol, double units, double price, string type, string date)
+        {
+            this.symbol = symbol;
+            this.units = units;
+            this.price = price;
+            this.type = type;
+            this.date = date;
+            next = null;
+        }
+    }
+}
