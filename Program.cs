4bab534fef83b3f09221c2755254b24a2386371a
pallavijawanjal25 <109516737+pallavijawanjal25@users.noreply.github.com>
2022-08-17 22:31:15 +0530

    StockManagement

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StockManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockAccount stock = new StockAccount("C:\\Users\\palla\\source\\repos\\184-RFP\\StockManagement\\StockManagement\\Portfolio.json");
+
+            Console.WriteLine(stock.welcome());
+            stock.menu();
+        }
+    }
+}
